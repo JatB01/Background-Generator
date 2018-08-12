ccd4a55e32856969283500beb51c85da6c33c2b4
JatB01 <38192190+JatB01@users.noreply.github.com>
2018-08-12 21:00:55 +0100

    added styles

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-var css = document.querySelector("h3");
-var h4 = document.querySelector("h4");
+var c1h = document.getElementById("c1h");
+var c2h = document.getElementById("c2h");
+var c1rgb = document.getElementById("c1rgb");
+var c2rgb = document.getElementById("c2rgb");
 var color1 = document.getElementById("color1");
 var color2 = document.getElementById("color2");
 var body = document.getElementById("gradient");
+var r1 = document.getElementById("r1");
+var r2 = document.getElementById("r2");
 var colors = ["#ff0000", "#00ff00", "#0000ff"];
 var btn = document.querySelector("button");
 
+//random hex generator
 function random_color() {
   var letters = "0123456789ABCDEF";
   var color = "#";
@@ -15,8 +20,19 @@ function random_color() {
   return color;
 }
 
+//hex to RGB converter
+function convertHex(hex) {
+  hex = hex.replace("#", "");
+  r = parseInt(hex.substring(0, 2), 16);
+  g = parseInt(hex.substring(2, 4), 16);
+  b = parseInt(hex.substring(4, 6), 16);
+
+  result = "rgb(" + r + " , " + g + " , " + b + ")";
+  return result;
+}
+
+//random button evt handler
 btn.onclick = function(evt) {
-  h4.textContent = "";
   var random_color1 = random_color();
   console.log(random_color1);
   var random_color2 = random_color();
@@ -25,18 +41,23 @@ btn.onclick = function(evt) {
   color2.value = random_color2;
   body.style.background =
     "linear-gradient(to right, " + random_color1 + ", " + random_color2 + ")";
-
-  css.textContent =
-    "Hex value: " + color1.value + " (color 1), " + color2.value + " (color2)";
+  c1h.textContent = random_color1;
+  c2h.textContent = random_color2;
+  c1rgb.textContent = convertHex(random_color1);
+  c2rgb.textContent = convertHex(random_color2);
+  r1.style.background = random_color1;
+  r2.style.background = random_color2;
 };
 
 function setGradient() {
-  h4.textContent = "";
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
-  css.textContent =
-    "Hex value: " + color1.value + " (color 1), " + color2.value + " (color2)";
+  c1h.textContent = color1.value;
+  c2h.textContent = color2.value;
+  c1rgb.textContent = convertHex(color1.value);
+  c2rgb.textContent = convertHex(color2.value);
+  r1.style.background = color1.value;
+  r2.style.background = color2.value;
 }
 
 color1.addEventListener("input", setGradient);
